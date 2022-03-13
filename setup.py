1f331508e8e8a2e3182dd39019bcfef81f8277a7
Trogiken <noah@blaszak.com>
2022-03-12 22:14:01 -0500

    Init Commit

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='package-SysEnv',
-    version='0.0.1',
+    version='0.0.2',
     license='MIT',
     author='Noah',
     author_email='noah.blaszak@gmail.com',
