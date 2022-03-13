129221b40ee2814147c4a613f79e6e3e7903f78a
Trogiken <noah@blaszak.com>
2022-03-13 13:28:35 -0400

    list all

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='sysenv',
-    version='0.1.4',
+    version='0.1.5',
     license='MIT',
     author='Noah',
     author_email='noah.blaszak@gmail.com',
