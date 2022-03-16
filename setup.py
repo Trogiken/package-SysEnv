96f94f8b69a9e28826174fdf9c16d94ae9dbc23a
Trogiken <noah@blaszak.com>
2022-03-15 21:39:19 -0400

    Remove reliance on own class method

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='sysenv',
-    version='0.2.8',
+    version='0.2.9',
     license='MIT',
     author='Noah',
     author_email='noah.blaszak@gmail.com',
